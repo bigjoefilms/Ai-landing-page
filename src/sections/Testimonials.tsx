3b8fe7a6a4787d26051f72ee3c9218023901b118
Bigjoe <bigjoefilm0@gmail.com>
2025-01-18 11:28:09 +0100

    Fix for testimonials

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -8,25 +8,25 @@ import {motion} from 'framer-motion'
 
 const testimonials = [
   {
-    text: "“This product has completely transformed how I manage my projects and deadlines.”",
+    text: "This product has completely transformed how I manage my projects and deadlines.",
     name: "Sophia Perez",
     title: "Director @ Quantum",
     avatarImg: avatar1,
   },
   {
-    text: "“These AI tools have completely revolutionized our SEO entire strategy overnight.”",
+    text: "These AI tools have completely revolutionized our SEO entire strategy overnight.",
     name: "Jamie Lee",
     title: "Founder @ Pulse",
     avatarImg: avatar2,
   },
   {
-    text: "“The user interface is so intuitive and easy to use, it has saved us countless hours.”",
+    text: "The user interface is so intuitive and easy to use, it has saved us countless hours.",
     name: "Alisa Hester",
     title: "Product @ Innovate",
     avatarImg: avatar3,
   },
   {
-    text: "“Our team's productivity has increased significantly since we started using this tool.”",
+    text: "Our team's productivity has increased significantly since we started using this tool.",
     name: "Alec Whitten",
     title: "CTO @ Tech Solutions",
     avatarImg: avatar4,
@@ -38,7 +38,7 @@ export const Testimonials = () => {
     <section className="py-20 md:py-24">
       <div className="container">
         <h2 className=" text-5xl md:text-6xl text-center tracking-tighter font-medium"> Beyond Expectation  </h2>
-        <p className="text-white/70 text-lg  md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">Our revoulutionary AI SEO tools have transformed our clients' strategies.</p>
+        <p className="text-white/70 text-lg  md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">Our revoulutionary AI SEO tools have transformed our clients strategies.</p>
         
       <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
         <motion.div

8ac7b1a559ea3633b2069c9af721f9c736563498
Bigjoe <bigjoefilm0@gmail.com>
2025-01-18 08:25:32 +0100

    fix testimonial.tsx page

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -8,25 +8,25 @@ import {motion} from 'framer-motion'
 
 const testimonials = [
   {
-    text: "“This product has completely transformed how I manage my projects and deadlines”",
+    text: "“This product has completely transformed how I manage my projects and deadlines.”",
     name: "Sophia Perez",
     title: "Director @ Quantum",
     avatarImg: avatar1,
   },
   {
-    text: "“These AI tools have completely revolutionized our SEO entire strategy overnight”",
+    text: "“These AI tools have completely revolutionized our SEO entire strategy overnight.”",
     name: "Jamie Lee",
     title: "Founder @ Pulse",
     avatarImg: avatar2,
   },
   {
-    text: "“The user interface is so intuitive and easy to use, it has saved us countless hours”",
+    text: "“The user interface is so intuitive and easy to use, it has saved us countless hours.”",
     name: "Alisa Hester",
     title: "Product @ Innovate",
     avatarImg: avatar3,
   },
   {
-    text: "“Our team's productivity has increased significantly since we started using this tool”",
+    text: "“Our team's productivity has increased significantly since we started using this tool.”",
     name: "Alec Whitten",
     title: "CTO @ Tech Solutions",
     avatarImg: avatar4,
